b6c3a68af912be1b93026c8926442a5d9929e9ba
krithika7402 <krithikan@presidio.com>
2023-10-19 14:45:57 +0530

    Login and Sign up implemented

--- app/src/main/java/com/example/socialmediaapp/main/CommonComponents.kt
+++ app/src/main/java/com/example/socialmediaapp/main/CommonComponents.kt
@@ -1,12 +1,19 @@
 package com.example.socialmediaapp.main
 
+import android.media.Image
 import android.os.Parcelable
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -15,16 +22,24 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.ImagePainter
+import coil.compose.ImagePainter.State.Empty.painter
+import coil.compose.rememberImagePainter
 import com.example.socialmediaapp.AppViewModel
 import com.example.socialmediaapp.DestinationScreen
+import com.example.socialmediaapp.R
 
 @Composable
 fun NotificationMessage(vm: AppViewModel) {
     val notificationState = vm.popupNotification.value
     val notificationMessage = notificationState?.getContentOrNull()
-    if(notificationMessage != null) {
+    if (notificationMessage != null) {
         Toast.makeText(LocalContext.current, notificationMessage, Toast.LENGTH_LONG).show()
     }
 }
@@ -68,5 +83,44 @@ fun navigateTo(navController: NavController, dest: DestinationScreen, vararg par
     navController.navigate(dest.route) {
         popUpTo(dest.route)
         launchSingleTop = true
+    }
+}
+
+@Composable
+fun CommonImage(
+    data: String?,
+    modifier: Modifier = Modifier.wrapContentSize(),
+    contentScale: ContentScale = ContentScale.Crop
+) {
+    val painter = rememberImagePainter(data = data)
+    Image(
+        painter = painter,
+        contentDescription = null,
+        modifier = modifier,
+        contentScale = contentScale
+    )
+    if (painter.state is ImagePainter.State.Loading) {
+        CommonProgressSpinner()
+    }
+}
+
+@Composable
+fun UserImageCard(
+    userImage: String,
+    modifier: Modifier = Modifier
+        .padding(8.dp)
+        .size(64.dp)
+) {
+    Card(shape = CircleShape, modifier = modifier) {
+        if (userImage.isNullOrEmpty()) {
+            Image(
+                painter = painterResource(id = R.drawable.baseline_user),
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(Color.Gray)
+            )
+        }
+        else {
+            CommonImage(data = userImage)
+        }
     }
 }
--- app/src/main/java/com/example/socialmediaapp/main/MyPostsScreen.kt
+++ app/src/main/java/com/example/socialmediaapp/main/MyPostsScreen.kt
@@ -1,22 +1,122 @@
 package com.example.socialmediaapp.main
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.socialmediaapp.AppViewModel
+import com.example.socialmediaapp.R
 
 @Composable
 fun MyPostsScreen(navController: NavController, vm: AppViewModel) {
-    Column(modifier = Modifier.fillMaxSize()) {
+    val userData = vm.userData.value
+    val isLoading = vm.inProgress.value
+    Column {
         Column(modifier = Modifier.weight(1f)) {
-            Text(text = "My Post Screen")
+            Row {
+                ProfileImage(userData?.imageUrl) {
+//                    newPostImageLauncher.launch("image/*")
+                }
+                Text(
+                    text = "15\nposts",
+                    modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "54\nlikes",
+                    modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "95\nfollowing",
+                    modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically),
+                    textAlign = TextAlign.Center
+                )
+            }
+            Column(modifier = Modifier.padding(8.dp)) {
+                val usernameDisplay =
+                    if (userData?.username == null) "" else "@${userData?.username}"
+                Text(userData?.name ?: "", fontWeight = FontWeight.Bold)
+                Text(text = usernameDisplay)
+                Text(text = userData?.bio ?: "")
+            }
+            OutlinedButton(
+                onClick = { },
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(10)
+            ) {
+                Text(text = "Edit Profile", color = Color.Black)
+            }
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Posts List")
+            }
         }
         BottomNavigationMenu(
             selectedItem = BottomNavigationItem.POSTS,
             navController = navController
         )
+    }
+    if(isLoading)
+        CommonProgressSpinner()
+}
+
+@Composable
+fun ProfileImage(imageUrl: String?, onClick: () -> Unit) {
+    Box(modifier = Modifier
+        .padding(top = 16.dp)
+        .clickable { onClick.invoke() }) {
+
+        UserImageCard(
+            userImage = imageUrl!!, modifier = Modifier
+                .padding(8.dp)
+                .size(80.dp)
+        )
+
+        Card(
+            shape = CircleShape,
+            border = BorderStroke(width = 2.dp, color = Color.White),
+            modifier = Modifier
+                .size(32.dp)
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 8.dp, end = 8.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.baseline_add),
+                contentDescription = null,
+                modifier = Modifier
+                    .background(Color.Blue)
+            )
+        }
     }
 }
